ec67d7911bd103e12cae23edc6a0ba8679f69bef
Kasia S <123kasia123@gmail.com>
2018-06-19 23:14:54 +0200

    Views

--- Projekt kurier/DB.cs
+++ Projekt kurier/DB.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 
 namespace Projekt_kurier
 {
@@ -10,12 +11,73 @@ namespace Projekt_kurier
     /// Klasa statyczna przechowująca listy użytkowników, kurierów i paczek, a także obiekt klasy Admin
     /// Chwilowo traktujemy ją jako bazę danych, później zdecyduje się czy zmieniamy to na bazę danych czy dodajemy metody zapisu i odczytu z pliku
     /// </summary>
-    static class DB
+    public class DB
     {
-        public static List<User> UsersList = new List<User>();
-        public static List<Courier> CouriersList = new List<Courier>();
-        public static Admin Administrator = new Admin("admin", "password");
+        private static Collection<User> UsersList = new ObservableCollection<User>();
+        private static Collection<Courier> CouriersList = new ObservableCollection<Courier>();
+        private static Admin Administrator = new Admin("admin", "password");
+        private static Collection<Package> PackagesList = new ObservableCollection<Package>();
 
-        public static List<Package> PackagesList = new List<Package>();
+        public DB() { }
+
+        private static DB singleton = null;
+        public static DB Instance
+        {
+            get
+            {
+                if (singleton == null)
+                    singleton = new DB();
+                return singleton;
+            }
+        }
+
+        public void AddUser (User item)
+        {
+            UsersList.Add(item);
+        }
+        public User GetUser(int id)
+        {
+            return UsersList[id];
+        }
+        public Collection<User> Users
+        {
+            get
+            {
+                return UsersList;
+            }
+        }
+
+        public void AddCourier(Courier item)
+        {
+            CouriersList.Add(item);
+        }
+        public Courier GetCourier (int id)
+        {
+            return CouriersList[id];
+        }
+        public Collection<Courier> Couriers
+        {
+            get
+            {
+                return CouriersList;
+            }
+        }
+
+        public void AddPackage(Package item)
+        {
+            PackagesList.Add(item);
+        }
+        public Package GetPackage(int id)
+        {
+            return PackagesList[id];
+        }
+        public Collection<Package> Packages
+        {
+            get
+            {
+                return PackagesList;
+            }
+        }
+
     }
 }
